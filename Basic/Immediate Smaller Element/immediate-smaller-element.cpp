8adec1e667d1cf670f8917ab1da14b0762baf5f4
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-02-09 19:09:45 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Immediate Smaller Element/immediate-smaller-element.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	void immediateSmaller(vector<int>&arr, int n) {
+	    //  code here
+	   // stack<int> st;
+	   // st.push(-1);
+	    
+	    for(int i=0;i<arr.size();i++)
+	    {
+	        int curr=arr[i];
+	        
+	        if(i+1<arr.size() && arr[i+1]<arr[i])
+	        {
+	            arr[i]=arr[i+1];
+	        }
+	        else
+	        {
+	            arr[i]=-1;
+	        }
+	        
+	    }
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.immediateSmaller(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
