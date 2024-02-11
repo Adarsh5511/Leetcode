6cbcccbac8105eae39116e1a57039a21905bfb46
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-02-11 18:01:44 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count the Reversals/count-the-reversals.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int countRev (string s);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        cout << countRev (s) << '\n';
+    }
+}
+
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+
+
+
+
+int countRev (string s)
+{
+    // your code here
+    
+    if(s.size() & 1) { return -1;}
+    
+    int count =0;
+    stack<char>st;
+    
+    for(int i=0;i<s.size();i++)
+    {
+        char ch=s[i];
+        
+        if(ch=='{')
+        {
+            st.push(ch);
+        }
+        else
+        {
+           if(!st.empty() && st.top()=='{')
+           {
+             
+               st.pop();
+           }
+           else
+           {
+               st.push(ch);
+         
+           }  
+            
+        }
+ 
+    }
+    
+    while(!st.empty())
+    {
+        char a=st.top(); st.pop();
+        char b=st.top(); st.pop();
+        
+        if(a==b)
+        {
+            count+=1;
+        }
+        else
+        {
+            count=count+2;
+        }
+    }
+  
+  return count;
+    
+}
+    
+   
+
