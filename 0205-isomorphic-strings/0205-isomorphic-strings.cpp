a76c9c11812f95a8c45afe026db4748c4499d269
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-04-02 18:39:44 +0530

    Time: 6 ms (65.28%), Space: 8.2 MB (77.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -23,11 +23,37 @@ public:
         return m;
      }
     
+    
+    string mapping(string m)
+    {
+        char start='a';
+        char mapping[128]={0};
+        for(int i=0;i<m.size();i++)
+        {
+            char ch=m[i];
+            if(mapping[ch]==0)
+            {
+                  mapping[ch]=start;
+                start++;
+            }
+            
+        }
+        for(int i=0;i<m.size();i++)
+        {
+              char ch=m[i];
+              m[i]=mapping[ch];
+        }
+        cout<<m;
+        return m;
+    }
+    
     bool isIsomorphic(string s, string t) {
         
 
-    string p=createmapping(s);
-    string a=createmapping(t); 
+    // string p=createmapping(s);
+    // string a=createmapping(t); 
+         string p=mapping(s);
+    string a=mapping(t); 
        
     if(p==a)
     {
