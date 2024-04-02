fef7eeb61a919d8f2867415789cc5a5c57c65234
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-04-02 18:30:01 +0530

    Time: 2 ms (92.78%), Space: 8.2 MB (77.12%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+     string createmapping(string m)
+     {
+         char start='a';
+         char mapping[128]={0};
+         for(int i=0;i<m.size();i++)
+         {
+             char ch=m[i];
+             if(mapping[ch]==0)
+             {
+                 mapping[ch]=start;
+                 start++;
+             }
+         }
+         for(int i=0;i<m.size();i++)
+         {
+             char ch=m[i];
+             m[i]=mapping[ch];
+         }
+        cout<<m;
+        return m;
+     }
+    
+    bool isIsomorphic(string s, string t) {
+        
+
+    string p=createmapping(s);
+    string a=createmapping(t); 
+       
+    if(p==a)
+    {
+        return true;
+    }
+    else
+     return false;
+
+    
+
+
+    }
+};
