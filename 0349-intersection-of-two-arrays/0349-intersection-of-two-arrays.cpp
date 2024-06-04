9963bcb8f279f97cd1f748d6a8d5c9241f0dcdaf
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-06-04 18:36:26 +0530

    Time: 8 ms (27.50%), Space: 13.4 MB (26.45%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,16 +1,52 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-         std::unordered_set<int> set1(nums1.begin(), nums1.end());
-        std::unordered_set<int> set2(nums2.begin(), nums2.end());
-
-        std::vector<int> result;
-        for (const auto& num : set1) {
-            if (set2.find(num) != set2.end()) {
-                result.push_back(num);
-            }
+     
+    unordered_map<int,int>count;
+    set<int>ans;
+        vector<int>ans1;
+        
+        for(int i=0;i<nums1.size();i++)
+        {
+            count[nums1[i]++];
         }
-
-        return result;
+        
+        
+      // unordered_map<int,int>::iterator it=count.begin();
+        
+        for(auto i:count)
+        {
+                 cout<<i.first<<" "<<i.second;
+        }
+        
+      for(int i=0;i<nums2.size();i++)
+      {
+        
+//           if(count.find(nums2[i])!=count.end())
+//           {
+              
+//           
+          
+          for(auto it=count.begin();it!=count.end();it++)
+          {
+              if(it->first==nums2[i])
+              {
+                  ans.insert(nums2[i]);
+              }
+              
+          }
+      }
+        
+        for(std::set<int>::iterator it=ans.begin();it!=ans.end();it++)
+        {
+            ans1.push_back(*it);
+        }
+        
+        return ans1;
+        
+        
+        
+        
+        
     }
 };
