450963402ec8c811c2c1d13b8c0460133cf39536
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-06-04 18:57:05 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (22.58%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -2,51 +2,65 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
      
-    unordered_map<int,int>count;
-    set<int>ans;
-        vector<int>ans1;
+    unordered_set<int>set1(nums1.begin(),nums1.end());
         
-        for(int i=0;i<nums1.size();i++)
+        unordered_set<int>common;
+        for(auto el:nums2)
         {
-            count[nums1[i]++];
+            if(set1.contains(el))
+            {
+              common.insert(el);
+            }
         }
         
+        vector<int>ans(common.begin(),common.end());
+//     
         
-      // unordered_map<int,int>::iterator it=count.begin();
-        
-        for(auto i:count)
-        {
-                 cout<<i.first<<" "<<i.second;
-        }
-        
-      for(int i=0;i<nums2.size();i++)
-      {
-        
-//           if(count.find(nums2[i])!=count.end())
-//           {
-              
-//           
-          
-          for(auto it=count.begin();it!=count.end();it++)
-          {
-              if(it->first==nums2[i])
-              {
-                  ans.insert(nums2[i]);
-              }
-              
-          }
-      }
-        
-        for(std::set<int>::iterator it=ans.begin();it!=ans.end();it++)
-        {
-            ans1.push_back(*it);
-        }
-        
-        return ans1;
+        return ans;
         
         
         
         
         
     }
+    
+    
+    // set<int>ans;
+//         vector<int>ans1;
+        
+//         for(int i=0;i<nums1.size();i++)
+//         {
+//             count[nums1[i]++];
+//         }
+        
+        
+//       // unordered_map<int,int>::iterator it=count.begin();
+        
+//         for(auto i:count)
+//         {
+//                  cout<<i.first<<" "<<i.second;
+//         }
+        
+//       for(int i=0;i<nums2.size();i++)
+//       {
+        
+// //           if(count.find(nums2[i])!=count.end())
+// //           {
+              
+// //           
+          
+//           for(auto it=count.begin();it!=count.end();it++)
+//           {
+//               if(it->first==nums2[i])
+//               {
+//                   ans.insert(nums2[i]);
+//               }
+              
+//           }
+//       }
+        
+//         for(std::set<int>::iterator it=ans.begin();it!=ans.end();it++)
+//         {
+//             ans1.push_back(*it);
+//         }
 };
