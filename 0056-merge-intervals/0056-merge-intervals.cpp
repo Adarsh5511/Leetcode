db140fae4c1cd66ed7d5f4503cac16fd0e9397ae
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-06-08 11:40:07 +0530

    Time: 26 ms (40.05%), Space: 22.6 MB (89.36%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>>ans;
+        
+        sort(intervals.begin(),intervals.end());
+        
+       
+        for(int i=0;i<intervals.size();i++)
+        {
+            int start=intervals[i][0];
+            int end=intervals[i][1];
+            if(!ans.empty() && end<=ans.back()[1] )
+            {
+                continue;
+            }
+            for(int j=i+1;j<intervals.size();j++)
+            {
+                    if(intervals[j][0]<=end)
+                    {
+                         end=max(end,intervals[j][1]);
+                         start=min(start,intervals[j][0]);
+                        
+                    }
+                else
+                {
+                    break;
+                }
+            }
+            
+            ans.push_back({start,end});
+           
+        }
+        return ans;
+        
+        
+        
+    }
+};
