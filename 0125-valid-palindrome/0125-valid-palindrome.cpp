2751103db444ece0c00476f86288e2fc9f411a69
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-06-09 12:49:34 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (61.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,60 +5,30 @@ public:
     
     bool isPalindrome(string s) { 
         
-//       int i=0;
-//       int j=s.size()-1;
+      int i=0;
+      int j=s.size()-1;
         
-//         while(i<=j)
-//         {
+        while(i<j)
+        {
             
-//         if((!isalpha(s[i])) )
-//                         {
-//                             i++;
-//                         }
-//          else if(!isalpha(s[j]) )
-//                     {
-//                         j--;
-//                     }
-            
-   
-//        else if (tolower(s[i])!=tolower(s[j]))
-//             {
-//                 return false;
-//            }
-//             else{
-                                 
-//                      i++;
-//                      j--;
-                
-//             }
-       
-         
-
-              
-
-            
-
-        
-      
-//       }
-        
-//         return true
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            if (!isalnum(s[left])) {
-                left++;
-            } else if (!isalnum(s[right])) {
-                right--;
-            } else if (tolower(s[left]) != tolower(s[right])) {
+        if(!isalnum(s[i]))
+                        {
+                            i++;
+                        }
+         else if(!isalnum(s[j]))
+                    {
+                        j--;
+                    }
+       else if (tolower(s[i])!=tolower(s[j]))
+            {
                 return false;
-            } else {
-                left++;
-                right--;
-            }
-        }
-
+           }
+            else{             
+                     i++;
+                     j--;
+            }     
+      }
+        
         return true;
     }
        
