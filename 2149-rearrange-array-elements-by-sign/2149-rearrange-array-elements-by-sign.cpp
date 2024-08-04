a5ed3965bb6e8800101847d6c8bc1edfce797b30
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-08-04 15:03:19 +0530

    Time: 132 ms (86.08%), Space: 143.2 MB (5.91%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,38 +2,31 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         
-        vector<int>arrneg;
-        vector<int>arrpos;
-        vector<int>nums1=nums;
-        vector<int>ans;
+       
+        vector<int>pos;
+        vector<int>neg;
+        vector<int>mix;
+        for(auto no:nums)
+        {
+            if(no>=0)
+            {
+                pos.push_back(no);
+            }
+            else{
+                neg.push_back(no);
+            }
+        }
+        
         int i=0;
         int j=0;
-        while(i<nums1.size())
-        {
-            if(nums1[i]>=0)
-            {
-                arrpos.push_back(nums1[i]);
-            }
-            else if(nums1[i]<0)
-            {
-                arrneg.push_back(nums1[i]);
-            }
-            i++;
-        }
-        int k=0;
-        int l=0;
-        while(j<nums.size()/2)
-        {
-            ans.push_back(arrpos[k]);
-            ans.push_back(arrneg[l]);
-            k++;
-            l++;
-            j++;
-            
-        }
-        
-        return ans;
-        
+       while(i<pos.size() || j<neg.size())
+       {
+           mix.push_back(pos[i]);
+           mix.push_back(neg[j]);
+           i++;
+           j++;
+       }
+        return mix;
         
         
     }
