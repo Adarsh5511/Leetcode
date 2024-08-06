40e982b17a2ce62b75217cccd57d52f14e43b534
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-08-06 14:30:45 +0530

    Time: 6 ms (39.31%), Space: 16.6 MB (25.82%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,47 +5,27 @@ public:
     
     int maxProduct(vector<int>& nums) {
      
-//         int maxi=nums[0];
-//         int prod=1;
-      
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             prod*=nums[i];
-//             maxi=max(prod,maxi);
-//             if(prod==0)
-//                 prod=1;
-//         }
-//         prod=1;
-        
-//          for(int i=nums.size()-1;i>=0;i--)
-//         {
-//             prod*=nums[i];
-//             maxi=max(prod,maxi);
-//             if(prod==0)
-//                 prod=1;
-//         }
-      
-//         return maxi;
-         double maxi = INT_MIN;
+        double maxi=nums[0];
         double prod=1;
-
+      
         for(int i=0;i<nums.size();i++)
         {
-          prod*=nums[i];
-          maxi=max(prod,maxi);
-          if(prod==0)
-           prod=1;
+            prod*=nums[i];
+            maxi=max(prod,maxi);
+            if(prod==0)
+                prod=1;
         }
         prod=1;
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-          prod*=nums[i];
-
-          maxi=max(prod,maxi);
-          if(prod==0)
-           prod=1;
-        }
-        return maxi;
         
+         for(int i=nums.size()-1;i>=0;i--)
+        {
+            prod*=nums[i];
+            maxi=max(prod,maxi);
+            if(prod==0)
+                prod=1;
+        }
+      
+        return maxi;
+      
     }
 };
