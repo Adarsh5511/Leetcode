f4cb1d666e05499a6715bf22570042d320f68f17
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-08-08 17:11:44 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (32.00%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -3,36 +3,30 @@ public:
     string removeOuterParentheses(string s) {
         
         string ans;
-        // stack<char>st;
-        int count=0;
+        stack<char>st;
+        
         
         for(int i=0;i<s.size();i++)
         {
-           if(s[i]=='(')
-           {
-               count+=1;
-               cout<<count<<" |";
-               
-               if(count!=1)
-               {
-                   ans.push_back(s[i]);
-               }
-              
-           }
-         else if(s[i]==')')
-          {
-             
-             count-=1;
-             cout<<count<<" --";
-             if(count!=0)
-             {
-                 ans.push_back(s[i]);
-             }
-             
-             
-          }
+            
+            
+            if(s[i]=='(')
+            {
+                if(!st.empty())
+                {
+                    ans.push_back(s[i]);
+                }
+                st.push(s[i]);
+            }
+            else{
+                st.pop();
+                if(!st.empty())
+                {
+                    ans.push_back(s[i]);
+                }
+            }
         }
-        
+       
         return ans;
         
         
