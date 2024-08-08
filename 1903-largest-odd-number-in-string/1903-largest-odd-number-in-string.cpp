2f341308f862a2f461deaf25d44df9717dad151b
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-08-08 18:00:29 +0530

    Time: 31 ms (28.63%), Space: 16.5 MB (41.26%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,22 +4,29 @@ public:
         
 
         
+        
+
+       
+        int maxindex=-1;
+        for(int i=num.size()-1;i>=0;i--)
+        {
+          int n=num[i]-'0';
+            if(n%2!=0)
+            {
+                maxindex=max(i,maxindex);
+            }
+        }
+        
+        if(maxindex==INT_MIN) return " ";
+        else
+            return num.substr(0,maxindex+1);
+        
        
         
      
-       for(int i=num.size();i>=0;i--)
-       {
-           char ch=num[i];
-           int no=ch-'0';
-           if(no%2==1)
-           {
-               return num.substr(0,i+1);
-               
-               break;
-           }
-       }
+
         
-        return "";
+        
         
         
     
