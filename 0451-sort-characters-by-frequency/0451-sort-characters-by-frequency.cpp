0047c78fdf1f0a34dd0d160ad934c20cb6cf4dab
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-08-10 16:05:47 +0530

    Time: 7 ms (89.57%), Space: 10.9 MB (67.33%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,50 +2,32 @@ class Solution {
 public:
     string frequencySort(string s) {
         
-//     unordered_map<char,int>mp;
-//         for(auto it:s)
-//         {
-//             mp[it]++;
-//         }
-//         vector<pair<char,int>>v;
+    unordered_map<char,int>mp;
+        for(auto it:s)
+        {
+            mp[it]++;
+        }
+        vector<pair<int,char>>v;
         
-//         for(auto it:mp)
-//         {
-//             v.push_back({it.first,it.second});
-//         }
+        for(auto it:mp)
+        {
+            v.push_back({it.second,it.first});
+        }
         
-//         sort(v.begin(),v.end(),greater<pair<char, int>>());
+        sort(v.begin(),v.end(),greater<pair<int,char>>());
         
-//         string ans;
-//         for(auto i:mp)
-//         {
-//             for(int k=0;k<i.second;k++)
-//             ans.push_back(i.first);
-//         }
+        string ans;
+        for(auto i:v)
+        {
+            for(int k=0;k<i.first;k++)
+            ans.push_back(i.second);
+        }
         
       
-//         return ans;
-          map<char, int> m;
-        for (int i = 0; i < s.length(); i++) {
-            m[s[i]]++;
-        }
-
-        vector<pair<int, char>> v;
-        for (auto it : m) {
-            v.push_back({it.second, it.first});
-        }
-
-        sort(v.begin(), v.end(), greater<pair<int, char>>());
-
-        string result = "";
-        for (auto it : v) {
-
-            for (int i = 0; i < it.first; i++) {
-                result += it.second;
-            }
-        }
-        return result;
+        return ans;
         
+        
+       
         
     }
 };
