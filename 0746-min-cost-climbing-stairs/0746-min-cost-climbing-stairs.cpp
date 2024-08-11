2f54876ee2935510fb2b21ed2ad959508242634c
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-08-11 14:15:30 +0530

    Time: 5 ms (52.31%), Space: 16.9 MB (16.55%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -5,25 +5,31 @@ public:
     {
         
         
-        if(index>cost.size()-1) return 0;
-        if(index==cost.size()-1) return cost[index];
+//         if(index>cost.size()-1) return 0;
+//         if(index==cost.size()-1) return cost[index];
+        
+        if(index<0) return 0;
+        if(index==0) return cost[index];
+        if(index==1) return cost[index];
+        
         
         if(dp[index]!=-1) return dp[index];
-        int left  =cost[index]+ mincost(index+1,cost,dp);
-        // int right=INT_MAX;
-        // if(index>1)
-         int right =cost[index]+mincost(index+2,cost,dp);
+        
+        int left  =cost[index]+ mincost(index-1,cost,dp);
+        
+
+         int right =cost[index]+mincost(index-2,cost,dp);
         
         return  dp[index]=min(left,right);
     }
     
     int minCostClimbingStairs(vector<int>& cost) {
         
-        int index=0;
+        int index=cost.size()-1;
         vector<int>dp(cost.size(),-1);
         int ans=mincost(index,cost,dp);
         
-          int index1=1;
+          int index1=cost.size()-2;
         vector<int>dp1(cost.size(),-1);
         int ans2=mincost(index1,cost,dp1);
         
