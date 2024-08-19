c4e3c7821e51b481d67e7fbe638570a9a3c0080f
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-08-19 15:50:22 +0530

    Time: 82 ms (45.01%), Space: 12 MB (29.51%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     
-    int min(int a, int b) {
- return (a < b) ? a : b;
-}
-
+    int solve(int n,vector<int>&dp){
+        
+        if(n==0){
+            return 0;
+        }
+        if(dp[n]!=-1) return dp[n];
+        int ans=n;
+        for(int i=1;i*i<=n;i++){
+            ans=min(ans,1+solve(n-i*i,dp));
+        }
+        
+        return dp[n]=ans;
+       
+    }
     
     int numSquares(int n) {
         
-                    vector<int> dp(n + 1, INT_MAX);
-             dp[0] = 0;
-             for (int i = 1; i <= n; i++) {
-                 
-             for (int j = 1; j * j <= i; j++) {
-             dp[i] = min(dp[i], dp[i - j * j] + 1);
-             }
-             }
-             return dp[n];
-
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
         
     }
 };
