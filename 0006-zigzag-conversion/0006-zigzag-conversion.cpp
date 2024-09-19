b611959768230d9578f39b019ee9ea6a7eb2f5ac
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-09-19 18:16:51 +0530

    Time: 7 ms (83.60%), Space: 13.2 MB (67.44%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,49 +1,71 @@
 class Solution {
 public:
-string convert(string s, int numRows) {
-  vector<string>zigzag(numRows);
-  bool direction=true;
-  int row=0;
-  int i=0;
-  if(numRows==1) return s;
+// string convert(string s, int numRows) {
+//   vector<string>zigzag(numRows);
+//   bool direction=true;
+//   int row=0;
+//   int i=0;
+//   if(numRows==1) return s;
   
-  while(true)
-  {
-    if(direction)
-    {
-     while(row<numRows && i<s.size())
-     {
-       zigzag[row].push_back(s[i]);
-       i++;
-       row++;
-     }
-     row=numRows-2;
-    }
-    else
-    {
-      while(row>=0 && i<s.size())
-     {
-       zigzag[row].push_back(s[i]);
-       i++;
-       row--;
-     }
-     row=1;
+//   while(true)
+//   {
+//     if(direction)
+//     {
+//      while(row<numRows && i<s.size())
+//      {
+//        zigzag[row].push_back(s[i]);
+//        i++;
+//        row++;
+//      }
+//      row=numRows-2;
+//     }
+//     else
+//     {
+//       while(row>=0 && i<s.size())
+//      {
+//        zigzag[row].push_back(s[i]);
+//        i++;
+//        row--;
+//      }
+//      row=1;
 
-    }
+//     }
 
-    if(i>=s.size()) break;
-    direction = !direction;
+//     if(i>=s.size()) break;
+//     direction = !direction;
    
  
 
-  }
-   string ans="";
-   for(int i=0;i<zigzag.size();i++)
-   {
-       ans +=zigzag[i];
-   }
+//   }
+//    string ans="";
+//    for(int i=0;i<zigzag.size();i++)
+//    {
+//        ans +=zigzag[i];
+//    }
      
-     return ans;
+//      return ans;
+// }
+    string convert(string s, int numRows) {
+    if (numRows == 1) return s;
+
+    vector<string> rows(min(numRows, int(s.length())));
+    int curRow = 0;
+    bool goingDown = false;
+
+    for (char c : s) {
+        rows[curRow] += c;
+        if (curRow == 0 || curRow == numRows - 1) {
+            goingDown = !goingDown;
+        }
+        curRow += goingDown ? 1 : -1;
+    }
+
+    string result;
+    for (const string& row : rows) {
+        result += row;
+    }
+
+    return result;
 }
    
 };
