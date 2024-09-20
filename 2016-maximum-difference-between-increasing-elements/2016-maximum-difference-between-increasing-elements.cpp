366473142edfb1b5c56bcd81bfeee8839439dbd2
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-09-21 00:39:08 +0530

    Time: 9 ms (15.40%), Space: 10.8 MB (79.64%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -2,31 +2,42 @@ class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
         
-//         int maxi=0;
-//         bool flag=false;
+        int maxi=0;
+        bool flag=false;
 //         for(int i=0;i<nums.size();i++){
 //             for(int j=i+1;j<nums.size();j++)
 //             {
+        
 //                 int diff=nums[j]-nums[i];
-//                 maxi=max(diff,maxi);
+//                   maxi=max(diff,maxi);
 //                 flag=true;
 //             }
 //         }
-//         if(flag)return true;
-        
-//         return -1;
-        
-          int maxi = 0;
-        bool flag = false;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(maxi<(nums[j]-nums[i])){
-                    maxi = nums[j]-nums[i];
-                    flag = true;
-                }
+//         if(flag){return maxi;}
+        for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            int diff = nums[j] - nums[i];
+            if (maxi < diff) {
+                maxi = diff;
+                flag = true;  // At least one valid difference is found
             }
         }
-        if(flag)return maxi;
+    }
+        if(flag){return maxi;}
+        
         return -1;
+        
+        //   int maxi = 0;
+        // bool flag = false;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(maxi<(nums[j]-nums[i])){
+        //             maxi = nums[j]-nums[i];
+        //             flag = true;
+        //         }
+        //     }
+        // }
+        // if(flag)return maxi;
+        // return -1;
     }
 };
