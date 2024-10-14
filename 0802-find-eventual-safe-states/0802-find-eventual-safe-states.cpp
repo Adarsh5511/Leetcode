41ef144842d14da5ee24b614293fd349c25b0c31
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-10-14 21:44:03 +0530

    Time: 148 ms (31.76%), Space: 65.6 MB (13.00%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,55 +1,77 @@
 class Solution {
 public:
     
-    bool dfs(int node,vector<int>&vis,vector<int>&pathvis,vector<vector<int>>& graph)
-    {
-           vis[node]=1;
-           pathvis[node]=1;
+//     bool dfs(int node,vector<int>&vis,vector<int>&pathvis,vector<vector<int>>& graph)
+//     {
+//            vis[node]=1;
+//            pathvis[node]=1;
            
         
-           for(auto ngbr:graph[node])
-           {
-               if(vis[ngbr]==0)
-               {
-                   if(dfs(ngbr,vis,pathvis,graph)==true)
-                   {
-                       return true;
-                   }
+//            for(auto ngbr:graph[node])
+//            {
+//                if(vis[ngbr]==0)
+//                {
+//                    if(dfs(ngbr,vis,pathvis,graph)==true)
+//                    {
+//                        return true;
+//                    }
                    
-               }
-               else if(vis[ngbr]==1 && pathvis[ngbr]==1)
-               {
-                   return true;
-               }
+//                }
+//                else if(vis[ngbr]==1 && pathvis[ngbr]==1)
+//                {
+//                    return true;
+//                }
                
-           }
+//            }
         
-        pathvis[node]=0;
+//         pathvis[node]=0;
         
-        return false;
-    }
+//         return false;
+//     }
+    
+  
     
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         
         int n=graph.size();
-        vector<int>vis(graph.size(),0);
-        vector<int>pathvis(graph.size(),0);
-        
-        vector<int>ans;
-        
-      
+        vector<int>indegree(n);
+        vector<vector<int>>adjreverse(n);
         
         for(int i=0;i<n;i++)
         {
-            
-                if(dfs(i,vis,pathvis,graph)==false)
-                {
-                    ans.push_back(i);
-                }
-            
+            for(auto ngbr:graph[i])
+            {
+                adjreverse[ngbr].push_back(i);
+                indegree[i]++;
+            }
         }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
             
-         
+            for(auto it:adjreverse[node])
+            {
+               indegree[it]--;
+               if(indegree[it]==0)
+               {
+                   q.push(it);
+               }
+            }
+        }
+      
+            
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
