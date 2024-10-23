fa753383ba9435787cd8e8026069cf739cbb1a85
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2024-10-23 21:36:44 +0530

    Time: 961 ms (58.64%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -8,7 +8,7 @@
 # ORDER BY percentage DESC, r.contest_id ASC;
 
 SELECT r.contest_id,
-        ROUND((COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users)) * 100, 2) AS percentage
+        ROUND((COUNT(DISTINCT r.user_id) / (SELECT COUNT(user_id) FROM Users)) * 100, 2) AS percentage
 FROM Register r
 JOIN Users u ON u.user_id = r.user_id
 GROUP BY r.contest_id
