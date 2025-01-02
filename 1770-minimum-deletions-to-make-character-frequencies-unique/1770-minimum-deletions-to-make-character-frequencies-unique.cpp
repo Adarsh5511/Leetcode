a0d84762677c5e39d9ed8e8100fd5058e1cd18ed
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2025-01-02 09:22:39 +0530

    Time: 11 ms (51.22%), Space: 20.2 MB (53.86%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
 
-    int check(vector<int>&freq,int &count,int size){
-    for(int i=0;i<26;i++)
-        {
+    // int check(vector<int>&freq,int &count,int size){
+    // for(int i=0;i<26;i++)
+    //     {
             
-            for(int j=0;j<26;j++)
-            {
-                if(i!=j)
-                {
-                    if(freq[i]==freq[j])
-                    {
-                        if(freq[i]!=0)
-                        {
-                            cout<<freq[i];
-                             freq[i]=freq[i]-1;
-                             count++;
-                            check(freq,count,size);
-                        }
+    //         for(int j=0;j<26;j++)
+    //         {
+    //             if(i!=j)
+    //             {
+    //                 if(freq[i]==freq[j])
+    //                 {
+    //                     if(freq[i]!=0)
+    //                     {
+    //                         cout<<freq[i];
+    //                          freq[i]=freq[i]-1;
+    //                          count++;
+    //                         check(freq,count,size);
+    //                     }
                        
-                    }
-                }
+    //                 }
+    //             }
                 
-            }
-        }
+    //         }
+    //     }
 
-     return count;
+    //  return count;
   
-    }
+    // }
 
 
     
    
     int minDeletions(string s) {
         
-      int size=0;
-      unordered_set<char>se;
-      for(auto i:s)
-      {
-          se.insert(i);
-      }
-      size=se.size();
-      cout<<size;
+     
           
 
          vector<int> freq(26, 0);
@@ -51,8 +44,27 @@ public:
             freq[s[i]-'a']++;
         }
         int count=0;
-        int ans=check(freq,count,size);
-        return ans;
+        // int ans=check(freq,count,26);
+        // return ans;
+
+        unordered_set<int>f;
+
+        for(int i=0;i<26;i++)
+        {
+            while(freq[i]>0 && f.count(freq[i]))
+            {
+                freq[i]--;
+                count++;
+            }
+            if(freq[i]>0)
+            {
+                f.insert(freq[i]);
+            }
+        }
+
+        return count;
+
+
         
 
 
