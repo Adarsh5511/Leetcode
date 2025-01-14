04d5c377fb98fb52ce102be20a3c3cfc33f0f4f9
Adarsh5511 <144539297+Adarsh5511@users.noreply.github.com>
2025-01-14 21:45:24 +0530

    Time: 11 ms (45.79%), Space: 88 MB (52.27%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -4,25 +4,24 @@ public:
         
         
 
-       set<int>st;
-       vector<int>ans(A.size());
-    
-       for(int i=0;i<A.size();i++)
-       {
-         
-            st.insert(A[i]);
-            int count=0;
-            for(int j=0;j<=i;j++)
-            {
-                if(st.contains(B[j]))
-                {
+     vector<int> ans(A.size());
+        unordered_set<int> setA;
+        
+      
+        for (int i = 0; i < A.size(); i++) {
+           
+            setA.insert(A[i]);
+            
+          
+            int count = 0;
+            for (int j = 0; j <= i; j++) {
+                if (setA.find(B[j]) != setA.end()) {
                     count++;
                 }
             }
-            ans[i]=count;
-
-         }
-       
-       return ans;
+            ans[i] = count;
+        }
+        
+        return ans;
     }
 };
